dbbf7f2fa4e2382939bbb34580d2cd3d8bd7e91a
Sonia Sheth <sheth.son@northeastern.edu>
2023-06-18 17:45:19 -0400

    started polishing up UI

--- src/main/java/cs3500/pa05/BujoMainStage.java
+++ src/main/java/cs3500/pa05/BujoMainStage.java
@@ -10,9 +10,16 @@ import cs3500.pa05.view.activities.ActivitiesButtons;
 import cs3500.pa05.view.tables.TaskQueueView;
 import cs3500.pa05.view.tables.WeekdayView;
 import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.layout.VBox;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 import java.util.List;
@@ -21,6 +28,8 @@ import java.util.List;
  * my playground to test stuff
  */
 public class BujoMainStage extends Application {
+  private int height = 1000;
+  private int width = 1000;
 
   private void initDummyData(WeekdayModel model) {
     model.getCategories().addAll(List.of(new Category("Work", null), new Category("School", null), new Category("Fun", null)));
@@ -37,12 +46,56 @@ public class BujoMainStage extends Application {
     try {
       WeekdayModel model = new WeekdayModel();
       this.initDummyData(model);
-      WeekdayView weekdayView = new WeekdayView();
-      TaskQueueView taskQueueView = new TaskQueueView();
+
+      int prefWidth = 1000;
+      int prefHeight = 400;
+      //elements needed for the whole grid
+      BorderPane bujo = new BorderPane();
+      bujo.setPrefSize(prefWidth, prefHeight);
+      bujo.setPadding(new Insets(20, 20, 20, 20));
+
+      //buttons needed
       ActivitiesButtons addActivities = new ActivitiesButtons();
       Button settings = new Button("Settings");
+      Button save = new Button("Save");
+
+      //week of label
+      HBox weekOfLabel = new HBox();
+//      weekOfLabel.setBorder
+//              (new Border
+//                      (new BorderStroke
+//                              (Color.BLACK, BorderStrokeStyle.SOLID, CornerRadii.EMPTY, BorderWidths.DEFAULT)));
+      Text weekOf = new Text("Week of 6/1 - 6/8");
+      weekOf.setFont(Font.font("verdana", FontWeight.BOLD, 20));
+      weekOfLabel.getChildren().add(weekOf);
+
+      //larger elements needed
+      WeekdayView weekdayView = new WeekdayView();
+      TaskQueueView taskQueueView = new TaskQueueView();
+
+      //put them all together
+      VBox left = new VBox(addActivities, taskQueueView);
+      left.setSpacing(20);
+
+      //set the top
+      HBox btn = new HBox(settings, save);
+      btn.setSpacing(15);
+      HBox top = new HBox(weekOfLabel, btn);
+      top.setSpacing(prefWidth - 200);
+      bujo.setTop(top);
+      BorderPane.setMargin(weekOfLabel, new Insets(10, 10, 100, 10));
+
+      //set the left
+      bujo.setLeft(left);
+      BorderPane.setMargin(left, new Insets(10, 10, 10, 10));
+      //set the center
+      bujo.setCenter(weekdayView);
+
+      //init the controller
       BujoController controller = new BujoController(primaryStage, model, weekdayView, taskQueueView, addActivities, settings);
-      Scene scene = new Scene(new VBox(addActivities, settings, weekdayView, taskQueueView));
+
+      //show the scene
+      Scene scene = new Scene(bujo);
       primaryStage.setScene(scene);
       primaryStage.show();
     } catch (Exception e) {
